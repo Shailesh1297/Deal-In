4120adb1ea7a2523de2fda4290164eaf2e3a23c6
shailesh <uic.17bca1297@gmail.com>
2020-02-14 14:40:20 +0530

    location activity searchbar,rounded corner rectangle,search icon

---
+++ app/src/main/java/com/example/dealin/location/Location.java
@@ -0,0 +1,16 @@
+package com.example.dealin.location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dealin.R;
+
+public class Location extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
