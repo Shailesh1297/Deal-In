0c5b51187acf77f4b949425d623751f1768e7263
pankaj <pankajsh9472@gmail.com>
2020-02-14 14:39:56 +0530

    added activity profile

---
+++ app/src/main/java/com/example/dealin/profile/Profile.java
@@ -0,0 +1,16 @@
+package com.example.dealin.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dealin.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
