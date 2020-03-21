1ca11dcae1637d292ec3d869302e08bc19fc4ee1
shailesh <uic.17bca1297@gmail.com>
2020-03-21 21:56:36 +0530

    modified admindashboard,profile,register & created splash screen

--- app/src/main/java/com/example/dealin/MainActivity.java
+++ app/src/main/java/com/example/dealin/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.dealin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.dealin.login.Login;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+             Intent intent=new Intent(getApplicationContext(), Login.class);
+             startActivity(intent);
+             finish();
+            }
+        },2000);
     }
 }
